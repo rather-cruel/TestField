8d1c7ce0e9c05570e7a78e960b86d63fde1a9a11
Artemii <art.fedotov.ua@gmail.com>
2023-06-02 16:29:48 +0200

    Concept Backup & update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import program.Ukrainian;
 
 public class Main {
     public static void main(String[] args) {
-        new Ukrainian("Привіт світ! нь н ьн 'нь56 ся");
+        new Ukrainian("Привіт Світ! нь н ьн 'нь56 ся");
+        new Ukrainian("Привіт Світ! дякую дякуючий");
+        new Ukrainian("Привіт Світ! як ся маєш?");
     }
 }
--- src/main/java/program/Ukrainian.java
+++ src/main/java/program/Ukrainian.java
@@ -13,33 +13,59 @@ public class Ukrainian {
         alphabet.put("г", "h");
         alphabet.put("ґ", "g");
         alphabet.put("д", "d");
+        alphabet.put("дь", "ď");
+        alphabet.put("дя", "ďa");
+        alphabet.put("дю", "ďu");
+        alphabet.put("дє", "ďe");
         alphabet.put("е", "e");
         alphabet.put("є", "je");
         alphabet.put("ж", "ž");
         alphabet.put("з", "z");
         alphabet.put("и", "y");
+        alphabet.put("ий", "ý");
         alphabet.put("і", "i");
         alphabet.put("ї", "ji");
         alphabet.put("й", "j");
         alphabet.put("к", "k");
         alphabet.put("л", "l");
+        alphabet.put("ль", "ľ");
+        alphabet.put("ля", "ľa");
+        alphabet.put("лю", "ľu");
+        alphabet.put("лє", "ľe");
         alphabet.put("м", "m");
         alphabet.put("н", "n");
-
         alphabet.put("нь", "ň");
-
+        alphabet.put("ня", "ňa");
+        alphabet.put("ню", "ňu");
+        alphabet.put("нє", "ňe");
         alphabet.put("о", "o");
         alphabet.put("п", "p");
+        // alphabet.put("при", "přy");
+        // alphabet.put("пре", "pře");
         alphabet.put("р", "r");
+        alphabet.put("рь", "ř");
         alphabet.put("с", "s");
 
         alphabet.put("ся", "sia");
+        alphabet.put("сє", "sie");
+        alphabet.put("сю", "siu");
+        // alphabet.put("сьо", "sío");
 
         alphabet.put("т", "t");
+        alphabet.put("ть", "ť");
+        alphabet.put("тя", "ťa");
+        alphabet.put("тє", "ťe");
+        alphabet.put("тю", "ťu");
         alphabet.put("у", "u");
         alphabet.put("ф", "f");
         alphabet.put("х", "ch");
         alphabet.put("ц", "c");
+
+        alphabet.put("ця", "cia");
+        alphabet.put("цє", "cie");
+        alphabet.put("цю", "ciu");
+        // alphabet.put("цьо", "cío");
+
         alphabet.put("ч", "č");
         alphabet.put("ш", "š");
         alphabet.put("щ", "šč");
@@ -52,15 +78,11 @@ public class Ukrainian {
 
     public static String convert(String message, HashMap<String, String> alphabet) {
         String output = "";
-
         for (int index = 0; index < message.length(); index++) {
             char nextLetter = 0;
             char currentLetter = message.charAt(index);
-
             boolean isCurrentUpper = Character.isUpperCase(currentLetter);
-
             String lowered = String.valueOf(Character.toLowerCase(currentLetter));
-
             String latinLetter = "";
 
             try {
@@ -82,7 +104,6 @@ public class Ukrainian {
 
             output += latinLetter;
         }
-
         return output;
     }
 }
